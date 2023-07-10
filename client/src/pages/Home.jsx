f3a4f2c0588a5503187a26316f30c9743bc69406
Brian Crowe <brian.j.crowe@protonmail.com>
2023-07-10 05:25:10 -0400

    refactor named react to client

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Irish Spirits</h1>
+      <form className="search">
+        <label htmlFor="gsearch">Search Product Database: </label>
+        <input type="search" id="product-search" className="product-search" />
+      </form>
+    </div>
+  )
+}
+
+export default Home
