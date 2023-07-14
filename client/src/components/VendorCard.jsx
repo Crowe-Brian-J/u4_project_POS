654bafb416bad490eda204d1aa91280661c6ecb7
Brian Crowe <brian.j.crowe@protonmail.com>
2023-07-14 11:20:59 -0400

    vendor delete button works

--- client/src/components/VendorCard.jsx
+++ client/src/components/VendorCard.jsx
@@ -3,12 +3,12 @@ const VendorCard = (props) => {
 
   return (
     <div className="vendor-card">
-      <h3>{props.vendorName}</h3>
+      <h3 className="name-wrapper">{props.vendorName}</h3>
       <div className="info-wrapper flex-col">
         <p>{props.repName}</p>
         <p>{props.phoneNumber}</p>
         <p>{props.email}</p>
-        <p>
+        <p className="button-container">
           <button>Edit</button>
         </p>
         <br />
--- client/src/pages/Vendors.jsx
+++ client/src/pages/Vendors.jsx
@@ -19,24 +19,38 @@ const Vendors = () => {
     getVendors()
   }, [])
 
+  const handleDelete = (id, list) => {
+    let expandList = [...list]
+    expandList.splice(id, 1)
+    setVendors(expandList)
+  }
+
   return (
     <div className="vendors container-grid">
       {/* remove index, change key to vendor.id */}
       {vendors.map((vendor) => (
-        <Link
-          // Don't use leading vendors/anything for link here.
-          to={`${vendor._id}`}
-          key={vendor._id}
-          style={{ textDecoration: 'none' }}
-        >
-          <VendorCard
+        <div key={vendor._id}>
+          <Link
+            // Don't use leading vendors/anything for link here.
+            to={`${vendor._id}`}
             key={vendor._id}
-            vendorName={vendor.vendorName}
-            repName={vendor.repName}
-            phoneNumber={vendor.phoneNumber}
-            email={vendor.email}
-          />
-        </Link>
+            style={{ textDecoration: 'none' }}
+          >
+            <VendorCard
+              key={vendor._id}
+              vendorName={vendor.vendorName}
+              repName={vendor.repName}
+              phoneNumber={vendor.phoneNumber}
+              email={vendor.email}
+            />
+          </Link>
+          <button
+            className="delete-button button-container"
+            onClick={() => handleDelete(vendor._id, vendors)}
+          >
+            Delete {vendor.vendorName}
+          </button>
+        </div>
       ))}
       <Link
         to={{ pathname: 'new', state: { vendors, setVendors } }}
